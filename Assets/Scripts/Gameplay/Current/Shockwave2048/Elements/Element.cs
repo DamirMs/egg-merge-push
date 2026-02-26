d513ee88774a4c54fd36cf45c782a9cfeaa71af8
Sodya <214626109+DamirMs@users.noreply.github.com>
2026-02-26 15:28:49 +0400

    Init

---
+++ Assets/Scripts/Gameplay/Current/Shockwave2048/Elements/Element.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace Gameplay.Current.Shockwave2048.Elements
+{
+    public class Element : MonoBehaviour, IMergeable
+    {
+        [SerializeField] private ElementView view;
+
+        public void Init(ElementData elementData)
+        {
+            view.Set(elementData.Sprite, (int)elementData.ElementType);
+        }
+        
+        public void PlayMergeEffect()
+        {
+            view.PlayMerge();
+        }
+
+        public void SetShopSprite(Sprite sprite)
+        {
+            view.SetSprite(sprite);
+        }
+
+        public void StopMergeEffect()
+        {
+            view.StopPlayingMerge();
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/Current/Shockwave2048/Elements/ElementView.cs
@@ -0,0 +1,37 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Gameplay.Current.Shockwave2048.Elements
+{
+    [Serializable]
+    public class ElementView
+    {
+        [SerializeField] private Image image;
+        [SerializeField] private Image mergeImage;
+        [SerializeField] private TextMeshProUGUI levelText;
+
+        public void Set(Sprite sprite, int level)
+        {
+            levelText.text = level.ToString();
+            
+            mergeImage.enabled = false;
+        }
+
+        public void SetSprite(Sprite sprite)
+        {
+            image.sprite = sprite;
+        }
+        
+        public void PlayMerge()
+        {
+            mergeImage.enabled = true;
+        }
+
+        public void StopPlayingMerge()
+        {
+            mergeImage.enabled = false;
+        }
+    }
+}
